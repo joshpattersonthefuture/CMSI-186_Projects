1d38d7f68f3ac9400425a37d2750a83d10fd30d5
joshpattersonthefuture <34358565+joshpattersonthefuture@users.noreply.github.com>
2018-01-16 00:38:15 -0800

    Create test.js
    Tests that it is only text

---
+++ homework00/test.js
@@ -0,0 +1,11 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * File name   : test.js
+ * Purpose     : Tests that it is only text
+ * Author      : Joshua Patterson
+ * Date        : 01/15/2018
+ * Description : None
+ * Notes       : None
+ * Warnings    : None
+ * Exceptions  : None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
+alert("Hello World!");
