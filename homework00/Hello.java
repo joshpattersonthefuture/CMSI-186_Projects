b5fdae58db0a32ed3055938d10e2c6cf160ab76f
joshpattersonthefuture <34358565+joshpattersonthefuture@users.noreply.github.com>
2018-01-16 00:26:01 -0800

    Create Hello.java
    Outputs "Hello World!"

---
+++ homework00/Hello.java
@@ -0,0 +1,16 @@
+/** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ *  File name     :  Hello.Java
+ *  Purpose       :  To output "Hello world!"
+ *  Author        :  Joshua Patterson
+ *  Date          :  2017-01-15
+ *  Description   :  Outputs the greeting "Hello World!"
+ *  Notes         :  None
+ *  Warnings      :  None
+ *  Exceptions    :  None
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
