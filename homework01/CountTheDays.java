f93fd3eae3620b189328c96aed3551b136926500
joshpattersonthefuture <34358565+joshpattersonthefuture@users.noreply.github.com>
2018-01-25 11:14:16 -0800

    update file

--- homework01/CountTheDays.java
+++ homework01/CountTheDays.java
@@ -5,6 +5,6 @@ public static void main ( String args[] ) {
       long month2 = Long.parseLong(args[3]);
       long day2   = Long.parseLong(args[4]);
       long year2  = Long.parseLong(args[5]);
-      long days = CalendarStuff.daysBetween( month1, day1, year1, month2, day2, year2 );
+      long days = CalendarStuff.daysBetween(month1, day1, year1, month2, day2, year2); //must be month/day/year...
            System.out.println(days + "\n days between the dates\n"); //possibly alldays to count leap year...
 }
