c669f31105ba09de017533504dc4fc28038b9062
joshpattersonthefuture <34358565+joshpattersonthefuture@users.noreply.github.com>
2018-04-19 10:42:55 -0700

    Update Fibonacci.java

--- homework06/Fibonacci.java
+++ homework06/Fibonacci.java
@@ -1,4 +1,22 @@
 public class Fibonacci {
+   private static final String usageMessage = "\n  You must enter an integer number....." +
+                                             "\n    Please try again!" +
+                                             "\n  USAGE: java Fibonacci <required_integer>\n\n";
+  private static int    maxCount    = 0;
+  private static int    working     = 15000;
+  private static String end1        = "st";
+  private static String end2        = "nd";
+  private static String end3        = "rd";
+  private static String endRest     = "th";
+  private static String cardinality = "";
+
+  private static final  int NO_CMD_LINE_ARGS = -1;
+  private static final  int BAD_CMD_LINE_ARG = -2;
+
+  public Fibonacci() {
+     super();
+  }
+   
    public static BrobInt Fibonacci(int x) {
       if (x <= 2) {
          return new BrobInt("1");
@@ -7,7 +25,7 @@ public class Fibonacci {
          BrobInt brob1 = new BrobInt("1");
          for (int i = 2; i < x; i++) {
             BrobInt brob2 = brob0.addInt(brob1);
-            System.out.println(brob2);
+            System.out.rintln(brob2);
             brob0 = brob1;
             brob1 = brob2;
          }
@@ -17,4 +35,5 @@ public class Fibonacci {
     public static void main(String[] args) {
        System.out.println(Fibonacci(Integer.parseInt(args[0])));
     }
+   
 }
